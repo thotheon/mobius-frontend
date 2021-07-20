767e70a6003c0dcaa9cfad54af6ed9ffd28e52b3
Dylan Mooers <dmooers@calpoly.edu>
2021-07-19 22:33:28 -0700

    Updating UI

--- src/components/SwapPage.tsx
+++ src/components/SwapPage.tsx
@@ -30,6 +30,7 @@ import { updateSwapAdvancedMode } from "../state/user"
 import { useActiveWeb3React } from "../hooks"
 import { useTranslation } from "react-i18next"
 import Footer from "../components/Footer"
+import arrow from "../assets/DownArrow.svg"
 
 interface Props {
   tokenOptions: {
@@ -133,7 +134,9 @@ const SwapPage = (props: Props): ReactElement => {
               isSwapFrom={true}
             />
           </div>
-          <div style={{ height: "48px" }}></div>
+        </div>
+        <img src={arrow} className="arrow" />
+        <div className="swapForm">
           <div className="row">
             <h3 className="swapTitle">{t("to")}</h3>
           </div>
@@ -149,43 +152,44 @@ const SwapPage = (props: Props): ReactElement => {
               isSwapFrom={false}
             />
           </div>
-          <div style={{ height: "24px" }}></div>
           {fromState.symbol && toState.symbol && (
-            <div className="row">
-              <div>
-                <span>{t("rate")}</span>
-                &nbsp;
-                <span>{exchangeRateInfo.pair}</span>
-                &nbsp;
-                <button
-                  className="exchange"
-                  onClick={onClickReverseExchangeDirection}
-                >
-                  <svg
-                    width="24"
-                    height="20"
-                    viewBox="0 0 24 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+            <>
+              <div className="row">
+                <div>
+                  <span>{t("rate")}</span>
+                  &nbsp;
+                  <span>{exchangeRateInfo.pair}</span>
+                  &nbsp;
+                  <button
+                    className="exchange"
+                    onClick={onClickReverseExchangeDirection}
                   >
-                    <path
-                      d="M17.4011 12.4196C17.4011 13.7551 16.5999 13.8505 16.4472 13.8505H6.62679L9.14986 11.3274L8.47736 10.6501L5.13869 13.9888C5.04986 14.0782 5 14.1991 5 14.3251C5 14.4511 5.04986 14.572 5.13869 14.6613L8.47736 18L9.14986 17.3275L6.62679 14.8044H16.4472C17.1054 14.8044 18.355 14.3274 18.355 12.4196V10.9888H17.4011V12.4196Z"
-                      fill="#3800D6"
-                    />
-                    <path
-                      d="M5.9539 7.58511C5.9539 6.24965 6.75519 6.15426 6.90781 6.15426H16.7283L14.2052 8.67733L14.8777 9.34984L18.2164 6.01117C18.3052 5.92181 18.355 5.80092 18.355 5.67492C18.355 5.54891 18.3052 5.42803 18.2164 5.33867L14.8777 2L14.2004 2.67727L16.7283 5.20035H6.90781C6.24962 5.20035 5 5.6773 5 7.58511V9.01597H5.9539V7.58511Z"
-                      fill="#3800D6"
-                    />
-                  </svg>
-                </button>
+                    <svg
+                      width="24"
+                      height="20"
+                      viewBox="0 0 24 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M17.4011 12.4196C17.4011 13.7551 16.5999 13.8505 16.4472 13.8505H6.62679L9.14986 11.3274L8.47736 10.6501L5.13869 13.9888C5.04986 14.0782 5 14.1991 5 14.3251C5 14.4511 5.04986 14.572 5.13869 14.6613L8.47736 18L9.14986 17.3275L6.62679 14.8044H16.4472C17.1054 14.8044 18.355 14.3274 18.355 12.4196V10.9888H17.4011V12.4196Z"
+                        fill="#3800D6"
+                      />
+                      <path
+                        d="M5.9539 7.58511C5.9539 6.24965 6.75519 6.15426 6.90781 6.15426H16.7283L14.2052 8.67733L14.8777 9.34984L18.2164 6.01117C18.3052 5.92181 18.355 5.80092 18.355 5.67492C18.355 5.54891 18.3052 5.42803 18.2164 5.33867L14.8777 2L14.2004 2.67727L16.7283 5.20035H6.90781C6.24962 5.20035 5 5.6773 5 7.58511V9.01597H5.9539V7.58511Z"
+                        fill="#3800D6"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <span className="exchRate">{formattedExchangeRate}</span>
               </div>
-              <span className="exchRate">{formattedExchangeRate}</span>
-            </div>
+              <div className="row">
+                <span>{t("priceImpact")}</span>
+                <span>{formattedPriceImpact}</span>
+              </div>
+            </>
           )}
-          <div className="row">
-            <span>{t("priceImpact")}</span>
-            <span>{formattedPriceImpact}</span>
-          </div>
           {formattedRoute && (
             <div className="row">
               <span>{t("route")}</span>
